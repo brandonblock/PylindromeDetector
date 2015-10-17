340db20b15d2ddde4a44ed45867f51727de1188f
manateemilitia <brandon.block@gmail.com>
2015-10-17 11:49:15 -0400

    Python3 changes

--- pal.py
+++ pal.py
@@ -6,13 +6,13 @@ punc = '''
        '''
 #Asks the user for a string and formats it for checking, the program ends if it matches the escape string.
 def query():
-    print "\nPlease enter your string:"
-    input_string = str(raw_input())
+    print("\nPlease enter your string:")
+    input_string = str(input())
     if input_string == escape:
         keep_going = False
     else:
-        input_string = input_string.translate(None, punc)
-	return input_string.lower()
+        input_string = input_string.translate(punc)
+        return input_string.lower()
 
 #Check to see if the string is a palindrome, omitting case, punctuation and spaces.
 def is_palindrome(x):
@@ -26,18 +26,18 @@ def is_palindrome(x):
 
 #Take the string, check if it's in the dictionary and add it and its palindrome state if not.
 def check_dict(x):
-    if x not in pal_dict.keys():
+    if x not in list(pal_dict.keys()):
         pal_dict[x] = is_palindrome(x)
-        print "New entry added"
+        print("New entry added")
     else:
-       print "Entry found in dictionary."
+       print("Entry found in dictionary.")
 
 
     if pal_dict[x]:
-        print "It's a palindrome!"
+        print("It's a palindrome!")
     else:
-        print "Not a palindrome!"
+        print("Not a palindrome!")
 
-print "Welcome to the palindrome detector! Type !q to quit."
+print("Welcome to the palindrome detector! Type !q to quit.")
 while keep_going:
     check_dict(query())
